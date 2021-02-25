d57cbf280ee6a9caf879d544a29cb8eab4ee7f03
lucasbf <lucasbf@gmail.com>
2021-02-25 11:07:52 -0300

    AULA 25-FEV

---
+++ Manha/Recursividade/exemplo1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+void conta(int i) {
+    cout << "Entrando: " << i << endl;
+    if(i < 100)
+        conta(i+1);
+    cout << "Saindo: " << i << endl;
+}
+
+int fat(int n) {
+    return n == 0 ? 1 : n*fat(n-1);    
+}
+
+int main(int argc, char *argv[])
+{
+    conta(0);  
+
+    cout << "Fat: " << fat(10) << endl; 
+    return 0;
+}
